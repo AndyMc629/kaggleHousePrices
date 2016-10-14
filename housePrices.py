ea76ee710b0f29b071b0c4c7787c89469e5a4d06
Andrew <apm13@Andrews-MBP.home>
2016-10-14 21:41:06 +0100

    Made a lot of progress, got my first good regression prediction

--- housePrices.py
+++ housePrices.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -10,7 +11,6 @@ from sklearn import preprocessing
 
 #read in data.
 data=pd.read_csv("Data/train.csv")
-
 # don't like get_dummies, creates a far
 # larger data frame and seems to get 
 # rid of pure numeric columns.
@@ -19,13 +19,22 @@ data=pd.read_csv("Data/train.csv")
 # to a pandas dataframe rather than a numpy
 # array. LabelEncoder could confuse ML algo though.
 
-#apply get dummies to rm categorical data
+#apply get dummies to transform categorical data
 data=pd.get_dummies(data)
+#remove all rows with NaNs.
+data.dropna(inplace=True) 
 
-print data.SalePrice
+#print data.SalePrice
+#print data.columns
+#print data['SalePrice','OverallQual']
 
+#plt.figure()
+#data.loc[:,['SalePrice','OverallQual']].hist()
+#plt.savefig("Some_hists.pdf")
+#plt.close()
 
 #print data
+
 ##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ## DATA CLEANED AND SUMMARY STATS PLOTTED
 ## APPLY ML ALGO
@@ -34,13 +43,32 @@ print data.SalePrice
 ##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ## NORMALISE, SPLIT INTO TEST/TRAIN
 ##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-#ds.normalise(data)
+ds.normalise(data)
 
-#convert train dataframe to numpy array
-#x_np_train=data.values[:,:-2].astype(np.float32)
-#y_np_train=data.values[:,-1].astype(np.float32)
+train=data.sample(frac=0.8,random_state=200)
+test=data.drop(train.index)
 
+#drop target variable and convert to numpy array.
+x_np_train=(train.drop('SalePrice',axis=1)).values.astype(np.float32)
+#print x_np_train
+#make target variable data a numpy array 
+y_np_train=(train['SalePrice']).values.astype(np.float32)
 #print y_np_train
+
+#drop target variable and convert to numpy array.
+x_np_test=(test.drop('SalePrice',axis=1)).values.astype(np.float32)
+#print x_np_train
+#make target variable data a numpy array 
+y_np_test=(test['SalePrice']).values.astype(np.float32)
+#print y_np_train
+
+
+
+#check for nans in dataframe
+#print data.isnull().values.any()
+#count number of nans in numpy array, '~' inverts array.
+#print np.count_nonzero(~np.isnan(x_np_train))
+#print np.count_nonzero(~np.isnan(y_np_train))
 ##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ## MODEL - NOTE: I PLAYED AROUND WITH A FEW FOR FUN.
 ##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -49,9 +77,9 @@ print data.SalePrice
 #from sklearn.cross_validation import *
 
 ##LINEAR REGRESSION
-#from sklearn import linear_model
+from sklearn import linear_model
 #create lin reg object.
-#linear=linear_model.LinearRegression()
+linear=linear_model.LinearRegression()
 ##ridge_regression
 #ridge=linear_model.Ridge(alpha=1.0)
 ##lasso_regression
@@ -77,7 +105,7 @@ print data.SalePrice
 ##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ## TRAINING AND CROSS-VALIDATION
 ##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-#print "LinReg: ", train_and_evaluate(linear,x_np_train,y_np_train), "\n"
+print "LinReg: ", ds.train_and_evaluate(linear,x_np_train,y_np_train,train,50000,'SalePrice'), "\n"
 #print "ridge: ", train_and_evaluate(ridge,x_np_train,y_np_train), "\n"
 #print "lasso: ", train_and_evaluate(lasso,x_np_train,y_np_train), "\n"
 #print "SVR: ", train_and_evaluate(svr,x_np_train,y_np_train)
@@ -85,16 +113,16 @@ print data.SalePrice
 #print "gpr: ", train_and_evaluate(gpr,x_np_train, y_np_train)
 
 ##Predict Output
-#predicted=linear.predict(x_np_test)
+predicted=linear.predict(x_np_test)
 
 ## Plot predicted vs true data.
-#plt.figure()
-#plt.scatter(y_np_test,predicted)
-#plt.plot([y_np_test.min(), y_np_test.max()], [y_np_test.min(), y_np_test.max()], 'k--', lw=4)
-#plt.xlabel('Measured')
-#plt.ylabel('Predicted')
-#plt.savefig("Prediction.pdf")
-#plt.close()
+plt.figure()
+plt.scatter(y_np_test,predicted)
+plt.plot([y_np_test.min(), y_np_test.max()], [y_np_test.min(), y_np_test.max()], 'k--', lw=4)
+plt.xlabel('Measured')
+plt.ylabel('Predicted')
+plt.savefig("Prediction.pdf")
+plt.close()
 
 ##Rerun with most strongly correlated features 
 ## - PropertyType and InLondon
@@ -118,6 +146,4 @@ print data.SalePrice
 #plt.savefig("Prediction.pdf")
 #plt.show()
 #plt.close()
-
-
 
