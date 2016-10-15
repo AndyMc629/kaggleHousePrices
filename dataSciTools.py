142f0296dd7a94214a5ed9f35bc786346ed1034b
Andrew <apm13@Andrews-MBP.home>
2016-10-15 12:24:43 +0100

    Tidied up in accordance with PEP8 styling using spyder.

--- dataSciTools.py
+++ dataSciTools.py
@@ -1,58 +1,60 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
-#Function to normalise data columns to range (0,1).
+# Function to normalise data columns to range (0,1).
 # takes a pandas dataframe as input, returns pd df
 # output.
+
+
 def normalise(df):
     result = df.copy()
     for feature_name in df.columns:
         max_value = df[feature_name].astype(np.float32).max()
         min_value = df[feature_name].astype(np.float32).min()
-        #only normalise if values are non-zero
-        if max_value!=0 or min_value!=0:
-            result[feature_name] = (df[feature_name].astype(np.float32) - min_value) / (max_value - min_value)
-    return result	
+        # only normalise if values are non-zero
+        if max_value != 0 or min_value != 0:
+            result[feature_name] = (
+                    df[feature_name].astype(np.float32) - min_value
+                    )/(max_value - min_value)
+    return result
 
-## general function to find indicex where condition is true.
+# general function to find indicex where condition is true.
+
+
 def find_indices(lst, condition):
     return [i for i, elem in enumerate(lst) if condition(elem)]
 
-## function to train the data and output Coefficient of det on train.
-## clf=sklearn model object.
-## Pass train dataframe in order to retrieve correct labels,
-## function will return plot coefficients for all features with
-## coefficients greater than importance threshold
-## also pass target variable now to make it more general.
-def train_and_evaluate(clf, X_train, y_train, train_df, importance_threshold, target_variable):
-    #perform training
+# function to train the data and output Coefficient of det on train.
+# clf=sklearn model object.
+# Pass train dataframe in order to retrieve correct labels,
+# function will return plot coefficients for all features with
+# coefficients greater than importance threshold
+# also pass target variable now to make it more general.
+
+
+def train_and_evaluate(clf, X_train, y_train, train_df,
+                       importance_threshold, target_variable):
+    # perform training
     clf.fit(X_train, y_train)
-    #print R^2
-    print "Coefficient of determination on training set:",clf.score(X_train, y_train)
-    #get labels, exclude target variable 
-    labels=list( (train_df.drop(target_variable,axis=1)).columns.values)
-    #make dict of features and coeff values
-    coeffs=dict(zip(labels,clf.coef_)) 
-    #trim the coeff dict, based on importance threshold
-    trimmed_coeffs={key: value for key, value in coeffs.iteritems() if (abs(value)>importance_threshold)}
+    # print R^2
+    print "Coefficient of determination on training set:",
+    clf.score(X_train, y_train)
+    # get labels, exclude target variable
+    labels = list((train_df.drop(target_variable, axis=1)).columns.values)
+    # make dict of features and coeff values
+    coeffs = dict(zip(labels, clf.coef_))
+    # trim the coeff dict, based on importance threshold
+    trimmed_coeffs = {key: value for key,
+                      value in coeffs.iteritems()
+                      if (abs(value) > importance_threshold)}
 
     print "Trimmed =", trimmed_coeffs
- 
+
     plt.figure()
-    plt.bar(range(len(trimmed_coeffs)), trimmed_coeffs.values(), align='center')
-    plt.xticks(range(len(trimmed_coeffs)),trimmed_coeffs.keys(), rotation=45)
+    plt.bar(range(len(trimmed_coeffs)),
+            trimmed_coeffs.values(),
+            align='center')
+    plt.xticks(range(len(trimmed_coeffs)), trimmed_coeffs.keys(), rotation=45)
     plt.tight_layout()
     plt.savefig("coeffs.pdf")
     plt.close()
-    #xtics = dataframe column names
-    
-    #print "labels",labels
-    
-    #x=np.linspace(1,Coeffs.size,Coeffs.size)
-    #plt.figure()
-    #plt.xlim(x[0]-0.5,x[-1]+0.5)
-    #plt.plot(x,Coeffs, linestyle='None',marker='D',markersize=10)
-    #plt.xticks(x,labels[1:]) #assumes 1st col is target variable.
-    #plt.savefig("Coeffs.pdf")
-    #plt.close()
-
--- housePrices.py
+++ housePrices.py
@@ -2,148 +2,160 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from pandas.tools.plotting import scatter_matrix
-import dataSciTools as ds 
-from sklearn import preprocessing
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+import dataSciTools as ds
+# LINEAR REGRESSION
+from sklearn import linear_model
+# SVM for REGRESSION
+# from sklearn import svm
+# RANDOM FORESTS for REGRESSION
+# from sklearn import ensemble
+# GAUSSIAN PROCESSES
+# from sklearn.gaussian_process import GaussianProcessRegressor
+# from sklearn.gaussian_process.kernels import WhiteKernel, ExpSineSquared, \
+# RBF, ConstantKernel as C
+# from sklearn import preprocessing
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # READ IN AND CLEAN DATA
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-#read in data.
-data=pd.read_csv("Data/train.csv")
+# read in data.
+data = pd.read_csv("Data/train.csv")
 # don't like get_dummies, creates a far
-# larger data frame and seems to get 
+# larger data frame and seems to get
 # rid of pure numeric columns.
 # sklearn's label_encoder seems better
-# - just working out how to apply this 
+# - just working out how to apply this
 # to a pandas dataframe rather than a numpy
 # array. LabelEncoder could confuse ML algo though.
 
-#apply get dummies to transform categorical data
-data=pd.get_dummies(data)
-#remove all rows with NaNs.
-data.dropna(inplace=True) 
+# apply get dummies to transform categorical data
+data = pd.get_dummies(data)
+# remove all rows with NaNs.
+data.dropna(inplace=True)
+# print data.SalePrice
+# print data.columns
+# print data['SalePrice','OverallQual']
 
-#print data.SalePrice
-#print data.columns
-#print data['SalePrice','OverallQual']
+# plt.figure()
+# data.loc[:, ['SalePrice', 'OverallQual']].hist()
+# plt.savefig("Some_hists.pdf")
+# plt.close()
 
-#plt.figure()
-#data.loc[:,['SalePrice','OverallQual']].hist()
-#plt.savefig("Some_hists.pdf")
-#plt.close()
+# print data
 
-#print data
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# DATA CLEANED AND SUMMARY STATS PLOTTED
+# APPLY ML ALGO
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-## DATA CLEANED AND SUMMARY STATS PLOTTED
-## APPLY ML ALGO
-##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-## NORMALISE, SPLIT INTO TEST/TRAIN
-##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# NORMALISE, SPLIT INTO TEST/TRAIN
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ds.normalise(data)
 
-train=data.sample(frac=0.8,random_state=200)
-test=data.drop(train.index)
+train = data.sample(frac=0.8, random_state=200)
+test = data.drop(train.index)
 
-#drop target variable and convert to numpy array.
-x_np_train=(train.drop('SalePrice',axis=1)).values.astype(np.float32)
-#print x_np_train
-#make target variable data a numpy array 
-y_np_train=(train['SalePrice']).values.astype(np.float32)
-#print y_np_train
+# drop target variable and convert to numpy array.
+x_np_train = (train.drop('SalePrice', axis=1)).values.astype(np.float32)
+# print x_np_train
+# make target variable data a numpy array
+y_np_train = (train['SalePrice']).values.astype(np.float32)
+# print y_np_train
 
-#drop target variable and convert to numpy array.
-x_np_test=(test.drop('SalePrice',axis=1)).values.astype(np.float32)
-#print x_np_train
-#make target variable data a numpy array 
-y_np_test=(test['SalePrice']).values.astype(np.float32)
-#print y_np_train
+# drop target variable and convert to numpy array.
+x_np_test = (test.drop('SalePrice', axis=1)).values.astype(np.float32)
+# print x_np_train
+# make target variable data a numpy array
+y_np_test = (test['SalePrice']).values.astype(np.float32)
+# print y_np_train
 
+# check for nans in dataframe
+# print data.isnull().values.any()
+# count number of nans in numpy array, '~' inverts array.
+# print np.count_nonzero(~np.isnan(x_np_train))
+# print np.count_nonzero(~np.isnan(y_np_train))
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# MODEL - NOTE: I PLAYED AROUND WITH A FEW FOR FUN.
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+# Uncomment below if you want kfold cross validation.
+# from sklearn.cross_validation import *
 
-#check for nans in dataframe
-#print data.isnull().values.any()
-#count number of nans in numpy array, '~' inverts array.
-#print np.count_nonzero(~np.isnan(x_np_train))
-#print np.count_nonzero(~np.isnan(y_np_train))
-##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-## MODEL - NOTE: I PLAYED AROUND WITH A FEW FOR FUN.
-##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# LINEAR REGRESSION
+# create lin reg object.
+linear = linear_model.LinearRegression()
+# ridge_regression
+# ridge=linear_model.Ridge(alpha=1.0)
+# lasso_regression
+# lasso=linear_model.Lasso(alpha=1.0)
+# Stochastic grad descrent regressor rather than closed form.
+# linear=linear_model.SGDRegressor(loss='squared_loss', penalty=None, \
+# random_state=42)
 
-##Uncomment below if you want kfold cross validation.
-#from sklearn.cross_validation import *
+# SVM for REGRESSION
+# from sklearn import svm
+# svr = svm.SVR(kernel='linear')
 
-##LINEAR REGRESSION
-from sklearn import linear_model
-#create lin reg object.
-linear=linear_model.LinearRegression()
-##ridge_regression
-#ridge=linear_model.Ridge(alpha=1.0)
-##lasso_regression
-#lasso=linear_model.Lasso(alpha=1.0)
-##Stochastic grad descrent regressor rather than closed form.
-#linear=linear_model.SGDRegressor(loss='squared_loss', penalty=None, random_state=42)
+# RANDOM FORESTS for REGRESSION
+# from sklearn import ensemble
+# extraTrees=ensemble.ExtraTreesRegressor(n_estimators=10, random_state=42)
 
-##SVM for REGRESSION
-#from sklearn import svm
-#svr = svm.SVR(kernel='linear')
+# GAUSSIAN PROCESSES
+# from sklearn.gaussian_process import GaussianProcessRegressor
+# from sklearn.gaussian_process.kernels import WhiteKernel, ExpSineSquared, \
+# RBF, ConstantKernel as C
+# kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
+# gpr=GaussianProcessRegressor(kernel=kernel,alpha=0,optimizer=None, \
+# normalize_y=True)
+# gpr.fit(x_np_train,y_np_train)
 
-##RANDOM FORESTS for REGRESSION
-#from sklearn import ensemble
-#extraTrees=ensemble.ExtraTreesRegressor(n_estimators=10, random_state=42)
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# TRAINING AND CROSS-VALIDATION
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+print "LinReg: ", ds.train_and_evaluate(linear, x_np_train, y_np_train, train,
+                                        50000, 'SalePrice'), "\n"
+# print "ridge: ", train_and_evaluate(ridge,x_np_train,y_np_train), "\n"
+# print "lasso: ", train_and_evaluate(lasso,x_np_train,y_np_train), "\n"
+# print "SVR: ", train_and_evaluate(svr,x_np_train,y_np_train)
+# print "Extra trees: ", train_and_evaluate(extraTrees, x_np_train,y_np_train)
+# print "gpr: ", train_and_evaluate(gpr,x_np_train, y_np_train)
 
-##GAUSSIAN PROCESSES
-#from sklearn.gaussian_process import GaussianProcessRegressor
-#from sklearn.gaussian_process.kernels import WhiteKernel, ExpSineSquared, RBF, ConstantKernel as C
-#kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
-#gpr=GaussianProcessRegressor(kernel=kernel,alpha=0,optimizer=None, normalize_y=True)
-#gpr.fit(x_np_train,y_np_train)
+# Predict Output
+predicted = linear.predict(x_np_test)
 
-##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-## TRAINING AND CROSS-VALIDATION
-##!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-print "LinReg: ", ds.train_and_evaluate(linear,x_np_train,y_np_train,train,50000,'SalePrice'), "\n"
-#print "ridge: ", train_and_evaluate(ridge,x_np_train,y_np_train), "\n"
-#print "lasso: ", train_and_evaluate(lasso,x_np_train,y_np_train), "\n"
-#print "SVR: ", train_and_evaluate(svr,x_np_train,y_np_train)
-#print "Extra trees: ", train_and_evaluate(extraTrees, x_np_train,y_np_train)
-#print "gpr: ", train_and_evaluate(gpr,x_np_train, y_np_train)
-
-##Predict Output
-predicted=linear.predict(x_np_test)
-
-## Plot predicted vs true data.
+# Plot predicted vs true data.
 plt.figure()
-plt.scatter(y_np_test,predicted)
-plt.plot([y_np_test.min(), y_np_test.max()], [y_np_test.min(), y_np_test.max()], 'k--', lw=4)
+plt.scatter(y_np_test, predicted)
+plt.plot(
+    [y_np_test.min(), y_np_test.max()],
+    [y_np_test.min(), y_np_test.max()], 'k--', lw=4)
 plt.xlabel('Measured')
 plt.ylabel('Predicted')
 plt.savefig("Prediction.pdf")
 plt.close()
 
-##Rerun with most strongly correlated features 
-## - PropertyType and InLondon
-##convert train dataframe to numpy array
-#x_np_train=train.values[:,[2,3,4]].astype(np.float32)
+# Rerun with most strongly correlated features
+# - PropertyType and InLondon
+# convert train dataframe to numpy array
+# x_np_train=train.values[:,[2,3,4]].astype(np.float32)
 
-##convert test dataframe to numpy array
-#x_np_test=test.values[:,[2,3,4]].astype(np.float32)
+# convert test dataframe to numpy array
+# x_np_test=test.values[:,[2,3,4]].astype(np.float32)
 
-#print "LinReg: ", train_and_evaluate(linear,x_np_train,y_np_train), "\n"
+# print "LinReg: ", train_and_evaluate(linear,x_np_train,y_np_train), "\n"
 
-##Predict Output
-#predicted=linear.predict(x_np_test)
+# Predict Output
+# predicted=linear.predict(x_np_test)
 
-## Plot predicted vs true data.
-#plt.figure()
-#plt.scatter(y_np_test,predicted)
-#plt.plot([y_np_test.min(), y_np_test.max()], [y_np_test.min(), y_np_test.max()], 'k--', lw=4)
-#plt.xlabel('Measured')
-#plt.ylabel('Predicted')
-#plt.savefig("Prediction.pdf")
-#plt.show()
-#plt.close()
-
+# Plot predicted vs true data.
+# plt.figure()
+# plt.scatter(y_np_test,predicted)
+# plt.plot(
+#    [y_np_test.min(), y_np_test.max()],
+#    [y_np_test.min(), y_np_test.max()], 'k--', lw=4)
+# plt.xlabel('Measured')
+# plt.ylabel('Predicted')
+# plt.savefig("Prediction.pdf")
+# plt.show()
+# plt.close()
